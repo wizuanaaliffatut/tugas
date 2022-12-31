b1f5a5230030c45be65c477d784d318b3da84798
wizuanaaliffatut <121333360+wizuanaaliffatut@users.noreply.github.com>
2023-01-01 01:59:08 +0700

    Update mycode.asm

--- mycode.asm
+++ mycode.asm
@@ -4,14 +4,14 @@ ORG 100h
 
 Data:
 JMP Tampildatadiri
-nama    DB "Nama    :wizuana aliffatut tadlqiroh",10d,13d,'$'
-nim     DB "Nim     :2200018327",10d,13d,'$' 
+nama    DB "Nama    :ana",10d,13d,'$'
+nim     DB "Nim     :22000",10d,13d,'$' 
 prodi   DB "Prodi   :informatika",10d,13d,'$'  
-ttl     DB "Ttl     :Gunungkidul,16-agustus-2003",10d,13d,'$'
-umur    DB "Umur    :19 tahun",10d,13d,'$'
+ttl     DB "Ttl     :surabaya,23-april-1999",10d,13d,'$'
+umur    DB "Umur    :23 tahun",10d,13d,'$'
 agama   DB "Agama   :islam",10d,13d,'$'
-alamat  DB "Alamat  :jl.imogiri timur",10d,13d,'$'
-email   DB "Email   :2200018327@webmail.uad.ac.id",10d,13d,'$'
+alamat  DB "Alamat  :jl.kamboja",10d,13d,'$'
+email   DB "Email   :ana@gmail.com",10d,13d,'$'
 
 Tampildatadiri:
 MOV AH,09h
